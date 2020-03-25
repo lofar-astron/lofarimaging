dfa83ea38f047e5bff5e02f27c2e62141912d7be
Tammo Jan Dijkema <dijkema@astron.nl>
2020-03-25 10:25:01 +0100

    Add h5py to requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
           packages=['lofarimaging'],
           url="https://github.com/lofar-astron/lofarimaging",
           requires=['numpy', 'numexpr', 'numba', 'astropy', 'lofargeotiff', 'lofarantpos',
-                    'matplotlib', 'folium', 'mercantile', 'owslib', 'packaging', 'Pillow'],
+                    'matplotlib', 'folium', 'mercantile', 'owslib', 'packaging', 'Pillow', 'h5py'],
           scripts=[],
           classifiers=[
               "Programming Language :: Python :: 3",
